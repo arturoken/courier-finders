07c7c64025c950f10d96f594ba7a8c3aec89647e
kendrik <kendrikgonzalez@datalink.com.ve>
2023-09-28 11:56:19 -0400

    añadiendo icono

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <head><link rel="icon" href="/favicon.ico" /></head>
+       <head><link rel="svg" href="/icon.svg" /></head>
       <body className={inter.className}>{children}</body>
     </html>
   )
